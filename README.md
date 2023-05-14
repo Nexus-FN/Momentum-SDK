c655403be905fa40e06e30a8f67deb287d349ab7
Finn <ernzerhofffinn@gmail.com>
2023-05-14 20:03:47 +0200

    More feautres + smoother experience + readme

---
+++ README.md
@@ -0,0 +1,47 @@
+![Banner](https://cdn.nexusfn.net/file/2023/05/Banner.png)
+
+[![pnpm](https://img.shields.io/badge/maintained%20with-pnpm-cc00ff.svg?style=for-the-badge&logo=pnpm)](https://pnpm.io/) 
+
+# Momentum SDK
+
+This is the official SDK for the Momentum Backend. It is written in TypeScript and is fully typed to ensure a smooth experience.
+
+## Installation
+
+```bash
+npm i momentumsdk
+pnpm i momentumsdk
+yarn add momentumsdk
+```
+
+## Usage
+
+```ts
+    //Import the SDK
+    import { Momentum } from 'momentumsdk'
+
+    //Create a new instance of the SDK
+    const momentum = new Momentum({
+        url: "https://apollo.nexusfn.net/api/",
+        apikey: "APIKEY"
+    })
+
+    //Get a user by their the provided key (username, email, accountId, discordId)
+    await momentum.getUser("username", "Zetax");
+
+    //Update a users fields by their key (username, email, accountId, discordId) with the new value
+    const updatedUser = momentum.updateUser("username", "Zetax", "email", "newemail@gmail.com");
+
+    //Get a users profile by their accountId
+    await momentum.getProfile("accountId", "1234567890");
+```
+
+## Support
+
+[![Discord](https://img.shields.io/discord/1097855449300213820?color=7289da&label=Discord&logo=discord&logoColor=white&style=for-the-badge)](https://discord.gg/3QxkZPKJ4K)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[ISC](https://choosealicense.com/licenses/isc/)
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+// src/momentum.ts
+
+interface MomentumOptions {
+    url: string;
+    apikey: string & { length: 32 };
+}
+
+type UserKey = "username" | "accountId" | "email" | "discordId";
+
+class Momentum {
+    private url: string;
+    private apikey: string;
+
+    constructor(options: MomentumOptions) {
+        this.url = options.url;
+        validateApiKey(options.apikey);
+        this.apikey = options.apikey;
+        if (!this.url) {
+            throw new Error('MomentumOptions.url is required');
+        }
+        if (!this.apikey) {
+            throw new Error('MomentumOptions.apikey is required');
+        }
+    }
+
+    public async getUser(key: UserKey, value: string) {
+
+        try {
+            await fetch(this.url + "/user/" + key + "/" + value, {
+                method: "GET",
+                headers: {
+                    "x-api-key": this.apikey,
+                }
+            })
+                .then((response: Response) => {
+                    return response.json();
+                });
+        } catch (error: any) {
+            throw new Error(error);
+        }
+
+    }
+
+    public async updateUser(key: UserKey, value: string, field: string, newValue: string) {
+
+        try {
+            await fetch(this.url + "/user/" + key + "/" + value, {
+                method: "POST",
+                headers: {
+                    "x-api-key": this.apikey,
+                },
+                body: JSON.stringify({
+                    fieldToUpdate: field,
+                    newValue: newValue
+                })
+            })
+                .then((response: Response) => {
+                    return response.json();
+                });
+        } catch (error: any) {
+            throw new Error(error);
+        }
+
+    }
+
+    public async getProfile(accountId: string, value: string) {
+
+        try {
+            await fetch(this.url + "/profile/accountId/" + accountId + "/" + value, {
+                method: "GET",
+                headers: {
+                    "x-api-key": this.apikey,
+                }
+            })
+                .then((response: Response) => {
+                    return response.json();
+                });
+        } catch (error: any) {
+            throw new Error(error);
+        }
+
+    }
+
+}
+
+function isApiKey(value: string): value is string & { length: 32 } {
+    return value.length === 32;
+}
+
+function validateApiKey(apikey: string): void {
+    if (!isApiKey(apikey)) {
+        throw new Error("Invalid API key. Please provide an API key that is exactly 32 characters long.");
+    }
+}
+
+export default Momentum;
